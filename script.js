1eaccef724feeb2198fc737df5def8ccd05e86a5
Ahid Abdurrahman Saleh <122637265+hidahid@users.noreply.github.com>
2023-04-05 17:00:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function satu(integer) {
+  //   console.log(integer);
+
+  let result = [];
+
+  for (let i = 1; i <= integer; i++) {
+    result.push(i);
+  }
+
+  return result;
+}
+
+function dua(arr) {}
+
+let number = 10;
+
+console.log(satu(number));
