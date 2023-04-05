fb1313c6b4967ecabb35fbf5b541f94c132fa6da
Ahid Abdurrahman Saleh <122637265+hidahid@users.noreply.github.com>
2023-04-05 17:05:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,22 @@ function satu(integer) {
   return result;
 }
 
-function dua(arr) {}
+function dua(integer) {
+  let result = 0;
+  let arrNumber = satu(integer);
+
+  for (let i = 0; i < arrNumber.length; i++) {
+    // console.log(arrNumber[i]);
+    let store = arrNumber[i];
+
+    if (store % 2 === 0) {
+      result += arrNumber[i];
+    }
+  }
+
+  return result;
+}
 
 let number = 10;
 
-console.log(satu(number));
+console.log(dua(number));
